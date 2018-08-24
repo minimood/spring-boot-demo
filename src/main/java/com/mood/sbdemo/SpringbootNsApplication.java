c480c02b091bcbbb7cb492c643db5424fd2f2de8
jinjun.zhang <jinjun.zhang@chebian.help>
2018-08-24 15:55:37 +0800

    first commit

---
+++ src/main/java/com/mood/sbdemo/SpringbootNsApplication.java
@@ -0,0 +1,12 @@
+package com.mood.sbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootNsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mood/sbdemo/SpringbootNsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mood.sbdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootNsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
